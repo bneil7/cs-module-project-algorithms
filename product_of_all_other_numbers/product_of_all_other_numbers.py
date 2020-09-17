9de697c5db1dee591b20daea418ca81948ed03c2
Brandon Neil <63384257+bneil7@users.noreply.github.com>
2020-09-16 19:43:29 -0600

    finished product_of_all_other_numbers and test passes

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,15 +2,38 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def product_of_all_other_numbers(arr):
-    # Your code here
 
-    pass
+
+def product_of_all_other_numbers(arr):
+    # create new array, make it equal to the number of indexes in the given array
+    array_length = len(arr)
+    new_array = [0] * array_length
+    # make a counter (initialize at zero)
+    counter = 0
+
+    # loop thru with range
+    # for loop, start at the index in front of current index
+    for i in range(len(arr)):
+        item = 1
+        for j in range(len(arr)):
+            if j != i:
+                # multiply all the numbers in the second for loop
+                item = item * arr[j]
+
+         # return that number and insert it into the created array at index of counter
+        new_array[counter] = item
+        # increment counter += 1
+        counter += 1
+    return new_array
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 2, 3, 4, 5]
+    # returns "Output of product_of_all_other_numbers: [120, 60, 40, 30, 24]"
 
-    print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8,
+    #        9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+
+    print(
+        f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,6 +2,15 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+
+# what if k is larger than the array ( len(nums) ) ?
+#   -- Go with a reasonable interpretation, and then follow through with it in your implementation..
+# if k > len(nums), then we'll find the overall max value of the nums array
+
+# does the return array need to be sorted?
+#   -- NO, since the prompt didn't specifically say so..
+
+
 def sliding_window_max(nums, k):
     # Your code here
 
@@ -9,8 +18,9 @@ def sliding_window_max(nums, k):
 
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
-    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    print(
+        f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
